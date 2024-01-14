5b5e31772a8cb10072225c3bfea9c427ebcc3abe
OnishYurii <onishchenkoyurii@gmail.com>
2024-01-14 19:09:58 +0200

    markup done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,16 @@
 import userData from '../userData.json';
 import friends from '../friends.json';
+import transactions from '../transactions.json';
 import { Profile } from './Profile';
 import { FriendList } from './FriendList';
+import { TransactionHistory } from './TransactionHistory';
 
 export const App = () => {
   return (
     <div>
       <Profile user={userData} />
       <FriendList items={friends} />
+      <TransactionHistory transactions={transactions} />
     </div>
   );
 };
